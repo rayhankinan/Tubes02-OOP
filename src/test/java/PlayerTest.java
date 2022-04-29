857412ce314b54ae1d90c2ba85b94c9310646941
fikrikhoironn <fikrikf470@gmail.com>
2022-04-29 21:08:18 +0700

    fix: playerTest and player

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -34,37 +34,4 @@ public class PlayerTest {
         assertEquals(79, player.getHealth(), 0.0);
     }
 
-    @Test
-    public void addToFieldTest() throws IOException, URISyntaxException, CardException {
-        Player player_1 = new Player("Rayhan", "deck_1.csv");
-        Character character_1 = CardDatabase.getCharacter(3);
-        player_1.addToField(0, character_1);
-        long status = 0;
-        if(player_1.getField(0) != null) {
-            status = 1;
-        }
-
-        assertEquals(1, status);
-    }
-
-    @Test
-    public void attackOpponentPlayerTest() throws IOException, URISyntaxException, CardException {
-        Player player_1 = new Player("Rayhan", "deck_1.csv");
-        Player player_2 = new Player("Munna", "deck_1.csv");
-        Character character_1 = CardDatabase.getCharacter(3);
-        player_1.addToField(0, character_1);
-        player_1.attackOpponentPlayer((SummonedCharacter) player_1.getField(0), player_2);
-        assertEquals(77, player_2.getHealth(), 0.0);
-    }
-
-    @Test
-    public void attackOpponentCardTest() throws IOException, URISyntaxException, CardException {
-        Player player_1 = new Player("Rayhan", "deck_1.csv");
-        Player player_2 = new Player("Munna", "deck_1.csv");
-        Character character_1 = CardDatabase.getCharacter(3);
-        player_1.addToField(0, character_1);
-        player_2.addToField(0,character_1);
-        player_1.attackOpponentCard((SummonedCharacter) player_1.getField(0), (SummonedCharacter) player_2.getField(0), player_2);
-        assertNull(player_2.getField(0));
-    }
 }
