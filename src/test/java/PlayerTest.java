1dbe4782669b30559f18631c496bcdf0618c662b
rayhankinan <rayhankinan@gmail.com>
2022-04-29 19:12:06 +0700

    Code cleaning

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -28,14 +28,14 @@ public class PlayerTest {
     }
 
     @Test
-    public void takeDamageTest() throws IOException, URISyntaxException, CardException, PlayerException {
+    public void takeDamageTest() throws IOException, URISyntaxException, CardException {
         Player player = new Player("Rayhan", "deck_1.csv");
         player.takeDamage(1);
         assertEquals(79, player.getHealth(), 0.0);
     }
 
     @Test
-    public void addToFieldTest() throws IOException, URISyntaxException, CardException, PlayerException {
+    public void addToFieldTest() throws IOException, URISyntaxException, CardException {
         Player player_1 = new Player("Rayhan", "deck_1.csv");
         Character character_1 = CardDatabase.getCharacter(3);
         player_1.addToField(0, character_1);
@@ -48,7 +48,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void attackOpponentPlayerTest() throws IOException, URISyntaxException, CardException, PlayerException {
+    public void attackOpponentPlayerTest() throws IOException, URISyntaxException, CardException {
         Player player_1 = new Player("Rayhan", "deck_1.csv");
         Player player_2 = new Player("Munna", "deck_1.csv");
         Character character_1 = CardDatabase.getCharacter(3);
